6e5c8a40181a2095bc3fb6f4dc273f1722fde3f9
Aleksey Pechurin <pe4urin@yandex.ru>
2022-09-09 13:33:53 +0300

    Рефакторинг js

--- js/script.js
+++ js/script.js
@@ -4,34 +4,26 @@ const menuBtn = document.querySelector('.burger');
 const menuOverlay = document.querySelector('.header__nav-overlay');
 const menuLink = document.querySelectorAll('.nav__link');
 
-menuBtn.onclick = function () {
-  menuBtn.classList.toggle('active');
+function showMenu() {
   menu.classList.toggle('active');
+  menuBtn.classList.toggle('active');
   menuOverlay.classList.toggle('active');
   document.body.classList.toggle('lock');
-};
+}
 
-menuOverlay.onclick = function() {
-  menuBtn.classList.toggle('active');
-  menu.classList.toggle('active');
-  menuOverlay.classList.toggle('active');
-  document.body.classList.toggle('lock');
-};
+menuBtn.addEventListener('click', showMenu);
 
-for(let i = menuLink.length; i--;) {
-  menuLink[i].onclick = function() {
-    menuBtn.classList.toggle('active');
-    menu.classList.toggle('active');
-    menuOverlay.classList.toggle('active');
-    document.body.classList.toggle('lock');
-  };
+menuOverlay.addEventListener('click', showMenu);
+
+for (const link of menuLink) {
+  link.addEventListener('click', showMenu);
 };
 
 //Accordion
 const accordion = document.querySelectorAll('.accordion__item');
 
 for(item of accordion) {
-  item.onclick= function() {
+  item.onclick = function() {
     this.classList.toggle('accordion__item--active');
   };
 };
